ea0ec147a1c4df79573106c4b6468502c19ff578
Philippe ROQUES-GEOFFROY <philippe.roquesgeoffroy@risf.io>
2023-06-23 11:42:53 +0200

    6.4 Accessing input files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
 import Cwm_expandable from "./components/Cwm_expandable";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <Cwm_expandable maxCharacters={50}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt rem
-        odit quis quaerat. In dolorem praesentium velit ea esse consequuntur cum
-        fugit sequi voluptas ut possimus voluptatibus deserunt nisi eveniet!
-      </Cwm_expandable>
+      <Form></Form>
     </>
   );
 }
